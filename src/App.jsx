8dd310bac041798354b15f6845b7af9b0b9e219e
gabriel <gabrielmedinatorres@hotmail.com>
2024-05-06 19:19:22 +0100

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./pages/home/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/homes" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
